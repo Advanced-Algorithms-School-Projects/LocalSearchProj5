99e8d5a16db14e281d296867d926d9bee53a5ad5
cassiels636 <35430596+cassiels636@users.noreply.github.com>
2018-06-22 14:24:49 -0400

    Knapsack 2

--- Project5/knapsack2p5.cpp
+++ Project5/knapsack2p5.cpp
@@ -1,5 +1,5 @@
-// Project 5 Knapsack 2: Solving knapsack using local search
-// and greedy algorithm
+// Project 5 Knapsack 2: Solving knapsack using simulated
+// annealing and greedy algorithm
 // Created by Patrick Hanbury and Cassandra Smith
 // Submitted 6/25/18
 //
@@ -21,6 +21,9 @@ using namespace std;
 
 void greedyKnapsack(knapsack &k);
 void orderKnapsack(knapsack &k, vector<int> &items);
+void simAnnealing(knapsack &k);
+knapsack getRandNeighbor(knapsack &k);
+bool chance(int th);
 
 int main()
 {
@@ -31,7 +34,7 @@ int main()
 	// Read the name of the graph from the keyboard or
 	// hard code it here for testing.
 
-	fileName = "knapsack/knapsack1024.input";
+	fileName = "knapsack/knapsack8.input";
 
 	//cout << "Enter filename" << endl;
 	//cin >> fileName;
@@ -50,6 +53,7 @@ int main()
 
 		greedyKnapsack(k);
 
+
 		//string output = "knapsack1024.output";
 		
 		cout << endl << "Best solution" << endl;
@@ -67,7 +71,6 @@ int main()
 
 	system("pause");
 }
-
 
 void greedyKnapsack(knapsack &k) {
 	vector<int> items;
@@ -100,5 +103,66 @@ void orderKnapsack(knapsack &k, vector<int> &items) {
 			j = j - 1;
 		}
 		items[j + 1] = temp;
+	}
+}
+
+void simAnnealing(knapsack &k) {
+	knapsack bestSol = knapsack(k);
+	//threshold value
+	float th = 1;
+	float alpha = 0.9;
+	
+	//number of times to increment
+	int totalIncrements = 30;
+	//increment num
+	int thIncrement = 0;
+
+	while (thIncrement <= totalIncrements) {
+		for (int i = 0; i < k.getNumObjects(); i++) {
+			knapsack temp = getRandNeighbor(k);
+			if (temp.getValue() > bestSol.getValue()) {
+				bestSol = temp;
+				k = temp;
+			}
+			else if (temp.getValue() > k.getValue()) {
+				k = temp;
+			}
+			else if (chance(th)) {
+				k = temp;
+			}
+		}
+		th *= alpha;
+		thIncrement += 1;
+	}
+	k = bestSol;
+}
+
+knapsack getRandNeighbor(knapsack &k) {
+	knapsack temp = knapsack(k);
+	vector<int> selected;
+	for (int i = 0; i < temp.getNumObjects(); i++) {
+		if (temp.isSelected(i)) {
+			selected.push_back(i);
+		}
+	}
+	srand(time(0));
+	int start = rand() % selected.size();
+
+	temp.unSelect(start);
+	for (int j = 0; j < temp.getNumObjects(); j++) {
+		if ((!temp.isSelected(j)) & ((temp.getCost() + temp.getCost(j)) <= temp.getCostLimit())) {
+			temp.select(j);
+		}
+	}
+	return temp;
+}
+
+bool chance(int th) {
+	srand(time(0));
+	if ((rand() % 1000) <= (th * 1000)) {
+		return true;
+	}
+	else {
+		return false;
 	}
 }
